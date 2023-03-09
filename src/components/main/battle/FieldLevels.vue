7cef2f2835428b874e0f24c4daa7e78ab67a8e8a
Lucas <lucas.prog07@gmail.com>
2023-03-09 22:37:23 +0000

    FieldLevels - show pro tips

--- src/components/main/battle/FieldLevels.vue
+++ src/components/main/battle/FieldLevels.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onBeforeMount, PropType, ref, watch } from 'vue';
+import { onBeforeMount, onMounted, PropType, ref, watch } from 'vue';
 import {
   delay,
   FieldConfig,
@@ -8,7 +8,7 @@ import {
   IBattleData,
   IMonsterCard,
   monsterCards,
-successProbability,
+  successProbability,
 } from '../../../scripts/main';
 import { playerStore } from '../../../scripts/store';
 import LockIcon from '../../icons/LockIcon.vue';
@@ -110,14 +110,18 @@ const startBattle = (level: number) => {
   if (!canBattle || isUnlocking.value) return;
 
   const { monsters } = fieldConfig.value.levels[level - 1];
-  const undiscoveredMonsters = monsters.filter(c => !playerStore.value.monsters.won.includes(c.id));
+  const undiscoveredMonsters = monsters.filter(
+    (c) => !playerStore.value.monsters.won.includes(c.id)
+  );
 
   let monsterList = monsters;
   // 90 probability to see a new monster instead of a monster you already defeated.
-  const showUndiscovered = undiscoveredMonsters.length > 0 && successProbability(90);
+  const showUndiscovered =
+    undiscoveredMonsters.length > 0 && successProbability(90);
   if (showUndiscovered) monsterList = undiscoveredMonsters;
 
-  const chosenMonster: IMonsterCard = monsterList[Math.floor(Math.random() * monsterList.length)];
+  const chosenMonster: IMonsterCard =
+    monsterList[Math.floor(Math.random() * monsterList.length)];
 
   const battleData: IBattleData = {
     field: props.field,
@@ -149,8 +153,12 @@ watch(
 );
 
 const discoveredPercentage = (level: number) => {
-  const monsterIds = fieldConfig.value.levels[level - 1].monsters.map(c => c.id);
-  const defeatedMonsters = playerStore.value.monsters.won.filter(id => monsterIds.includes(id)).length;
+  const monsterIds = fieldConfig.value.levels[level - 1].monsters.map(
+    (c) => c.id
+  );
+  const defeatedMonsters = playerStore.value.monsters.won.filter((id) =>
+    monsterIds.includes(id)
+  ).length;
   return Math.round((defeatedMonsters / monsterIds.length) * 100);
 };
 
@@ -162,6 +170,32 @@ const unlock = async (level: number) => {
   await delay(0.5);
   isUnlocking.value = false;
 };
+
+const tips: string[] = [
+  "Maybe it's better to battle several times on the same level until you get stronger.",
+  "When you have enough gold, visit the store. Who knows, maybe you'll get stronger there?",
+  "Have you checked if you don't have better weapons or attacks to equip yourself with?",
+  'Did you discover 100% of each level?',
+];
+
+const isShowingTip = ref<boolean>(false);
+const chosenTip = ref<string>();
+
+const changeTip = async () => {
+  isShowingTip.value = false;
+  await delay(1);
+  const filteredTips = tips.filter((t) => chosenTip.value != t);
+  const randomIndex = Math.floor(Math.random() * filteredTips.length);
+  chosenTip.value = filteredTips[randomIndex];
+  isShowingTip.value = true;
+};
+
+watch(chosenTip, async () => {
+  await delay(15);
+  await changeTip();
+});
+
+onMounted(changeTip);
 </script>
 
 <template>
@@ -172,9 +206,19 @@ const unlock = async (level: number) => {
     <div
       class="z-10 absolute flex flex-col p-5 w-full h-full justify-center items-center"
     >
-      <h1 class="text-2xl absolute bottom-10">
+      <h1 class="text-2xl absolute top-10 left-10">
         <button @click="$emit('back')">Go back</button>
       </h1>
+
+      <Transition name="fade">
+        <div
+          class="text-xl absolute bottom-10 px-10 py-2 bg-gray-800 rounded text-gray-400"
+          v-if="!!chosenTip && isShowingTip && !isUnlocking"
+        >
+          PRO TIP: <span class="font-light">{{ chosenTip }}</span>
+        </div>
+      </Transition>
+
       <div class="h-full w-full flex justify-around items-center">
         <button
           class="w-32 h-32 bg-gray-800 rounded-lg text-xl ring ring-gray-900 bg-cover bg-center relative flex items-center justify-center disabled:opacity-40 transition-all"
@@ -185,13 +229,19 @@ const unlock = async (level: number) => {
             ' w-40 h-40': level.boss,
           }"
           v-for="level in fieldConfig.levels"
-          :disabled="playerStore.story[field].currentLevel < level.number || playerStore.story[field].currentLevel != level.number && isUnlocking"
+          :disabled="
+            playerStore.story[field].currentLevel < level.number ||
+            (playerStore.story[field].currentLevel != level.number &&
+              isUnlocking)
+          "
           @click="startBattle(level.number)"
         >
           <Transition name="slide-left" mode="out-in">
             <div v-if="level.isEnabled" class="flex flex-col gap-2">
               <h1 class="text-2xl">{{ level.name }}</h1>
-              <h2 class="text-xs uppercase text-gray-500 font-bold">{{ discoveredPercentage(level.number) }}% DISCOVERED</h2>
+              <h2 class="text-xs uppercase text-gray-500 font-bold">
+                {{ discoveredPercentage(level.number) }}% DISCOVERED
+              </h2>
             </div>
             <div v-else>
               <Transition name="fade" mode="out-in">
